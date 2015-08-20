7fb27e54a330cbfbe366729700fb12c581419849
mira <lngm1@hotmail.com>
2015-08-19 22:49:12 -0700

    Add total CPU segment

--- top.lua
+++ top.lua
@@ -9,6 +9,10 @@ function update_top(text_widget)
   local proc_status = fd:read("*all")
   fd:close()
 
+  fd = io.popen("mpstat | awk '$3 ~ /CPU/ { for(i=1;i<=NF;i++) { if ($i ~ /%idle/) field=i } } $3 ~ /all/ { print 100 - $field }'")
+  local total_cpu_status = fd:read('*all')
+  fd:close()
+
   -- Get %CPU, %MEM, TIME+, and process name
   local proc_substr = string.match(proc_status,
     "(%d?%d%.%d.*%w+)")
@@ -20,15 +24,19 @@ function update_top(text_widget)
   proc_name = proc_name:gsub("%.", "")
   proc_name = proc_name:gsub(":", "")
   proc_name = proc_name:sub(0, 12)
+  total_cpu_status:gsub("%%", "")
   num_proc_spaces = 12 - proc_name:len()
   num_cpu_spaces = 4 - cpu:len()
   num_mem_spaces = 4 - mem:len()
+  num_total_cpu_spaces = 6 - total_cpu_status:len()
   proc_padding = string.rep(" ", num_proc_spaces)
   cpu_padding = string.rep(" ", num_cpu_spaces)
   mem_padding = string.rep(" ", num_mem_spaces)
+  total_cpu_padding = string.rep(" ", num_total_cpu_spaces)
   proc_str = " Proc: " .. proc_name .. proc_padding
     .. " CPU: " .. cpu_padding .. cpu
-    .. "  Mem: " .. mem_padding .. mem
+    .. " Mem: " .. mem_padding .. mem
+    .. " | Total CPU: " .. total_cpu_padding .. total_cpu_status
 
   text_widget:set_markup(proc_str)
 end
