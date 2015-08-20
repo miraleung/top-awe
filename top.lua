32156b6fd54aa26b60a99e0700e6e6a10fce5a1d
mira <lngm1@hotmail.com>
2015-08-19 22:56:32 -0700

    Adjust proc string formatting

--- top.lua
+++ top.lua
@@ -9,7 +9,9 @@ function update_top(text_widget)
   local proc_status = fd:read("*all")
   fd:close()
 
-  fd = io.popen("mpstat | awk '$3 ~ /CPU/ { for(i=1;i<=NF;i++) { if ($i ~ /%idle/) field=i } } $3 ~ /all/ { print 100 - $field }'")
+  local mpstat_cmd = "mpstat | awk '$3 ~ /CPU/ { for(i=1;i<=NF;i++) { "
+    .. "if ($i ~ /%idle/) field=i } } $3 ~ /all/ { print 100 - $field }'"
+  fd = io.popen(mpstat_cmd)
   local total_cpu_status = fd:read('*all')
   fd:close()
 
@@ -24,7 +26,6 @@ function update_top(text_widget)
   proc_name = proc_name:gsub("%.", "")
   proc_name = proc_name:gsub(":", "")
   proc_name = proc_name:sub(0, 12)
-  total_cpu_status:gsub("%%", "")
   num_proc_spaces = 12 - proc_name:len()
   num_cpu_spaces = 4 - cpu:len()
   num_mem_spaces = 4 - mem:len()
@@ -34,8 +35,8 @@ function update_top(text_widget)
   mem_padding = string.rep(" ", num_mem_spaces)
   total_cpu_padding = string.rep(" ", num_total_cpu_spaces)
   proc_str = " Proc: " .. proc_name .. proc_padding
-    .. " CPU: " .. cpu_padding .. cpu
-    .. " Mem: " .. mem_padding .. mem
+    .. " CPU: " .. cpu_padding .. cpu .. "%"
+    .. " Mem: " .. mem_padding .. mem .. "%"
     .. " | Total CPU: " .. total_cpu_padding .. total_cpu_status
 
   text_widget:set_markup(proc_str)
